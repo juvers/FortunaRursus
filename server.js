2b3f13f7eaeac5e83c8af7d437ff45cb77b4092b
Jude kuti <judekuti@gmail.com>
2020-08-16 12:41:17 -0500

    Use a different port as most ports are in use

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8087;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
