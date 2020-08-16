ef6df24b3c81da16d92bf50c5bc66e3b3b09dc2a
Jude kuti <judekuti@gmail.com>
2020-08-16 12:17:24 -0500

    Establish association between users and roles as many-to-many and sync in server.js

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors())
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
 
+app.use(cors())
+    // app.use(cors(corsOptions));
+
+//  app.use(function(req, res, next) {
+//      res.header("Access-Control-Allow-Origin", "*");
+//      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//      next();
+// });
+
+// app.options('*', cors())
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -14,10 +26,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync().then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
+
+
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the fortune application." });
 });
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
